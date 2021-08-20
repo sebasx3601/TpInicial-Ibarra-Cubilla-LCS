9cec1c4c87c870d9d66ecd479e753d6f78c7ce70
sebasx3601 <sebastianx3600@gmail.com>
2021-08-19 23:06:27 -0300

    Se agregaron los metodos para TipoContactoDTO
    Se agregaron los metodos getters y setters para tipo contacto

--- agenda/src/main/java/dto/TipoContactoDTO.java
+++ agenda/src/main/java/dto/TipoContactoDTO.java
@@ -1,5 +1,21 @@
 package dto;
 
 public class TipoContactoDTO {
-
+	
+	private int idContacto;
+	private String nombreTipoContacto;
+	
+	public int getIdContacto() {
+		return idContacto;
+	}
+	public void setIdContacto(int idContacto) {
+		this.idContacto = idContacto;
+	}
+	public String getNombreTipoContacto() {
+		return nombreTipoContacto;
+	}
+	public void setNombreTipoContacto(String nombreTipoContacto) {
+		this.nombreTipoContacto = nombreTipoContacto;
+	}
+	
 }
