95283447746569543ae663c8794267fae6a945ed
sebasx3601 <sebastianx3600@gmail.com>
2021-08-20 01:56:57 -0300

    En agenda se declaro para el tipo de contacto y metodo de enlistar
    Falta declarar en agenda la funcion para agregar y borrar tipos de contactos

--- agenda/src/main/java/modelo/Agenda.java
+++ agenda/src/main/java/modelo/Agenda.java
@@ -2,17 +2,22 @@ package modelo;
 
 import java.util.List;
 import dto.PersonaDTO;
+import dto.TipoContactoDTO;
 import persistencia.dao.interfaz.DAOAbstractFactory;
 import persistencia.dao.interfaz.PersonaDAO;
+import persistencia.dao.interfaz.TipoContactoDAO;
 
 
 public class Agenda 
 {
 	private PersonaDAO persona;	
+
+	private TipoContactoDAO tiposDeContacto;
 	
 	public Agenda(DAOAbstractFactory metodo_persistencia)
 	{
 		this.persona = metodo_persistencia.createPersonaDAO();
+		this.tiposDeContacto =  metodo_persistencia.createTipoContactoDAO();
 	}
 	
 	public void agregarPersona(PersonaDTO nuevaPersona)
@@ -29,5 +34,10 @@ public class Agenda
 	{
 		return this.persona.readAll();		
 	}
+	
+	public List<TipoContactoDTO> obtenerTiposDeContacto()
+	{
+		return this.tiposDeContacto.readAll();		
+	} 
 	
 }
