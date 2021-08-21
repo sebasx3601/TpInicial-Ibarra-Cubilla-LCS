b9dc00a823adbf5143a34e949747069dd95808ba
sebasx3601 <sebastianx3600@gmail.com>
2021-08-21 04:30:48 -0300

    Se programo el insert delete read de provincia
    Se programo los metodos para insertar borrar y leer de provincia

--- agenda/src/main/java/persistencia/dao/mysql/UbicacionDAOSQL.java
+++ agenda/src/main/java/persistencia/dao/mysql/UbicacionDAOSQL.java
@@ -108,18 +108,86 @@ public class UbicacionDAOSQL implements UbicacionDAO
 	}
 	
 	public boolean insertProvincia(ProvinciaDTO provincia) {
-		return true;
+		PreparedStatement statement;
+		Connection conexion = Conexion.getConexion().getSQLConexion();
+		boolean isInsertExitoso = false;
+		try
+		{
+			statement = conexion.prepareStatement(insertProvincia);
+			statement.setInt(1, provincia.getId());
+			statement.setString(2, provincia.getNombreProvincia());
+			statement.setInt(3, provincia.getIdPais());
+			
+			if(statement.executeUpdate() > 0)
+			{
+				conexion.commit();
+				isInsertExitoso = true;
+			}
+		} 
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+			try {
+				conexion.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+		}
+		
+		return isInsertExitoso;
 	}
 
 	public boolean deleteP(ProvinciaDTO provincia_a_eliminar) {
-		return true;
+		PreparedStatement statement;
+		Connection conexion = Conexion.getConexion().getSQLConexion();
+		boolean isdeleteExitoso = false;
+		try 
+		{
+			statement = conexion.prepareStatement(deleteProvincia);
+			statement.setString(1, Integer.toString(provincia_a_eliminar.getId()));
+			if(statement.executeUpdate() > 0)
+			{
+				conexion.commit();
+				isdeleteExitoso = true;
+			}
+		} 
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
+		return isdeleteExitoso;
 	}
 	
 	public List<ProvinciaDTO> readAllProvincia(){
+		PreparedStatement statement;
+		ResultSet resultSet; //Guarda el resultado de la query
 		List<ProvinciaDTO> res = new ArrayList<ProvinciaDTO>();
+		Conexion conexion = Conexion.getConexion();
+		try 
+		{
+			statement = conexion.getSQLConexion().prepareStatement(readallProvincia);
+			resultSet = statement.executeQuery();
+			while(resultSet.next())
+			{
+				System.out.println(getProvinciaDTO(resultSet).getId() + " " + getProvinciaDTO(resultSet).getNombreProvincia() + " " + getProvinciaDTO(resultSet).getIdPais());
+				res.add(getProvinciaDTO(resultSet));
+			}
+		} 
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
 		return res;
 	}
 	
+	private ProvinciaDTO getProvinciaDTO(ResultSet resultSet) throws SQLException 
+	{
+		int id = resultSet.getInt("IdProvincia");
+		String nombre = resultSet.getString("NombreProvincia");
+		int idPais = resultSet.getInt("IdPais");
+		return new ProvinciaDTO(id, idPais, nombre);
+	}
+
 	public boolean insertLocalidad(LocalidadDTO localidad) {
 		return true;
 	}
