df00e8452e04baa4326251ef7673d429e3ee205f
sebasx3601 <sebastianx3600@gmail.com>
2021-08-29 03:35:23 -0300

    Se agrego en el reporte mas datos del contacto

--- agenda/src/main/java/main/Main.java
+++ agenda/src/main/java/main/Main.java
@@ -26,12 +26,12 @@ public class Main
 		
 		Vista vista = new Vista();
 		Agenda modelo = new Agenda(new DAOSQLFactory());
-		modelo.agregarPersona(new PersonaDTO(0,"Sebastian","550436","","","","",1,"","1999-05-27",1,1));
-		modelo.agregarPersona(new PersonaDTO(0,"Pehuen","02320 456123","","","","",2,"","1995-03-13",1,1));
-		modelo.agregarPersona(new PersonaDTO(0,"Norma","123572","","","","",5,"","1998-01-12",1,1));
-		modelo.agregarPersona(new PersonaDTO(0,"Juan","442923","","","","",7,"","1995-09-22",1,1));
-		modelo.agregarPersona(new PersonaDTO(0,"Francisco","442923","","","","",7,"","1995-09-22",1,1));
-		modelo.agregarPersona(new PersonaDTO(0,"Manuel","442923","","","","",7,"","1995-09-22",1,1));
+		modelo.agregarPersona(new PersonaDTO(0,"Sebastian","550436","Jose Hernandez","1234","piso 1","No dept",1,"sebas@hotmail.com","1999-05-27",1,1));
+		modelo.agregarPersona(new PersonaDTO(0,"Pehuen","02320 456123","Ituzaingo","1009","3","-",2,"Pehuen@gmail.com","1995-03-13",1,1));
+		modelo.agregarPersona(new PersonaDTO(0,"Norma","123572","Calle falsa","1274","Uno","Dos",5,"NormaMaria@hotmail.com","1998-01-12",1,1));
+		modelo.agregarPersona(new PersonaDTO(0,"Juan","442923","Tres tristes","6833","1","",7,"JuanJose@gmail.com","1995-09-22",1,1));
+		modelo.agregarPersona(new PersonaDTO(0,"Francisco","442923","Green Hills","1232","1","",7,"NoMeHables@gmail.com","1995-09-22",1,1));
+		modelo.agregarPersona(new PersonaDTO(0,"Manuel","442923","Uruguay","135","","",7,"No tiene","1995-09-22",1,1));
 		Controlador controlador = new Controlador(vista, modelo);
 		controlador.inicializar();
 		
